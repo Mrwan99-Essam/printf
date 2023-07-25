2d7090f2f3ae47b671f40a92b7d22f463b3732f7
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-07-25 07:05:15 -0700

    number.c module

--- number.c
+++ number.c
@@ -4,7 +4,7 @@
  * convert - converter function, a clone of itoa
  * @num: number
  * @base: base
- * @flag: argument flag
+ * @flags: argument flag
  * @params: parameter struct
  *
  * Return: string
@@ -34,4 +34,50 @@ char *convert(long int num, int base, int flags, params_t *params)
 	} while (n != 0);
 
 	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
 
+/**
+ * print_usigned - print unsigned integer number
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+*/
+
+int print_usigned(va_list ap, params_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(1, 10, CONVERT_UNSIGNED, params), params));
+}
+
+/**
+ * print_address - prints address
+ * @ap: arguments pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+*/
+
+int print_address(va_list ap, params_t *params)
+{
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)");
+
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
+}
