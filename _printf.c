047aabe8023012fb4b79779860270349df43f11c
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-07-25 11:43:28 -0700

    update

--- _printf.c
+++ _printf.c
@@ -28,4 +28,25 @@ int _printf(const char *format, ...)
 			sum += _putchar(*p);
 			continue;
 		}
+		start = p;
+		p++;
+		while (get_flag(p, &params))
+		{
+			p++;
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_form_to(start, p, params.l_modifier ||
+					params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
+}
+
 
